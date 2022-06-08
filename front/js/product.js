cdc3d18f7b271fb4f34cbc234cc7b5e07e3dcdd3
Solène Viogeat <so.gd@hotmail.fr>
2022-06-08 12:18:48 +0200

    création et répartition des données de l'api dans le dom pour la page d'accueil

---
+++ front/js/product.js
@@ -0,0 +1,12 @@
+/* 4- Faire le lien entre un produit de la page d'accueil et la page Produit */
+
+/** “URLSearchParams” :
+var str = "https://waytolearnx.com/t.html?name=alex-babtise&age=25&address=paris";
+var url = new URL(str);
+var name = url.searchParams.get("name");
+console.log(name);
+*/
+
+//Créer une nouveau URL contenant l'id du produit correspondant
+
+
